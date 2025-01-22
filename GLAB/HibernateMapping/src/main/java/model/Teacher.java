5daaeae0d111343c753d0bee813fa0dccc3e8992
noor188 <noormmlk@gmail.com>
2025-01-22 10:50:40 -0700

    GLAB 305.5.1B & 305.5.2B

---
+++ GLAB/HibernateMapping/src/main/java/model/Teacher.java
@@ -0,0 +1,80 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+public class Teacher implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int teacherId;
+    private String TeacherName;
+    private String salary;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private Department department;
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name="address_id")
+    private Address address;
+
+    public Teacher(){}
+
+    public Teacher(int teacherId, String TeacherName, String salary) {
+        super();
+        this.teacherId = teacherId;
+        this.TeacherName = TeacherName;
+        this.salary = salary;
+    }
+
+    public Teacher(String salary, String TeacherName) {
+        super();
+        this.salary = salary;
+        this.TeacherName = TeacherName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getTeacherId() {
+        return this.teacherId;
+    }
+
+    public String getTeacherName() {
+        return TeacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        TeacherName = teacherName;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
